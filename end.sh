a726c12732dbb1dce3a8502fc6ffdb07574018ed
zytls92 <zytls92@gmail.com>
2022-09-27 17:17:43 +0900

    modify end.sh

--- end.sh
+++ end.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 kill -9 `ps -ef|grep SNAPSHOT.jar|awk '{print $2}'` > /dev/null 2>&1 &
-cd deploy/libs
+cd libs
 nohup java -jar *SNAPSHOT.jar > /dev/null 2>&1 &
 sudo service nginx reload
